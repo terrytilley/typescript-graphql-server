0490720b3d22d5bba2732d4395608d70802f1fb0
Terry Tilley <terry@terrytilley.com>
2018-12-23 21:09:22 +0000

    redis session store

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,16 @@ import "reflect-metadata";
 
 import * as express from "express";
 import * as session from "express-session";
+import * as connectRedis from "connect-redis";
 import { createConnection } from "typeorm";
 import { ApolloServer } from "apollo-server-express";
 
+import redis from "./redis";
 import typeDefs from "./graphql/typeDefs";
 import resolvers from "./graphql/resolvers";
+import { redisSessionPrefix } from "./constants";
+
+const RedisStore = connectRedis(session as any);
 
 const initServer = async () => {
   await createConnection();
@@ -24,9 +29,19 @@ const initServer = async () => {
 
   app.use(
     session({
+      name: "sid",
       secret: process.env.SECRET as string,
       resave: false,
-      saveUninitialized: false
+      saveUninitialized: false,
+      store: new RedisStore({
+        client: redis as any,
+        prefix: redisSessionPrefix
+      }),
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
+      }
     })
   );
 
